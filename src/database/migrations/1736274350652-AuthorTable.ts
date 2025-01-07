17f43977cb7ea86dd26e136741967a3813660cd8
Lucas da Silva <sklucassilva@gmail.com>
2025-01-07 15:49:22 -0300

    cria a tabela Author com colunas para id, nome, data de nascimento, biografia, nacionalidade e timestamps

--- src/database/migrations/1736274350652-AuthorTable.ts
+++ src/database/migrations/1736274350652-AuthorTable.ts
@@ -1,11 +1,55 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class AuthorTable1736274350652 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'author',
+        columns: [
+          {
+            name: 'id',
+            type: 'serial',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'birth_date',
+            type: 'date',
+          },
+          {
+            name: 'biography',
+            type: 'text',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'nationality',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('author');
+  }
 }
