ad169fc1aeebc5fee11db9e510acd11069faac90
lppellin <lucaspaolopellin@gmail.com>
2025-01-08 15:35:07 -0300

    cria entidade para tabela livros

--- src/entities/Livro.ts
+++ src/entities/Livro.ts
@@ -1,6 +1,31 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-class Livro {
- 
+@Entity("livros")
+export default class Livro {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "varchar", length: 150 })
+  title: string;
+
+  @Column({ type: "text", nullable: true })
+  description: string;
+
+  @Column({ type: "date" })
+  publication_date: Date;
+
+  @Column({ type: "varchar", length: 13 })
+  isbn: string;
+
+  @Column({ type: "int" })
+  page_count: number;
+
+  @Column({ type: "varchar", length: 100 })
+  language: string;
+
+  @CreateDateColumn({ type: "timestamp" })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: "timestamp" })
+  updated_at: Date;
 }
-
-export default Livro;
