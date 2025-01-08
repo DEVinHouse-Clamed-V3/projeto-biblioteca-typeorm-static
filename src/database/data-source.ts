500b37cd797d14065caea0c430e77424e236f696
lppellin <lucaspaolopellin@gmail.com>
2025-01-08 10:08:00 -0300

    conecta database

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
     host: "localhost",
     port: 5432,
     username: "postgres",
-    password: "admin",
+    password: "0182",
     database: "biblioteca",
     synchronize: true,
     logging: true,
